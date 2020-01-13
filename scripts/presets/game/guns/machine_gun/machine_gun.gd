99fef2f3a64d122937add09c865214a7eb9420d3
TernaryFish <sad.coder@yandex.ru>
2020-01-13 22:01:18 +0600

    create basik stand_enemy

--- scripts/presets/game/guns/machine_gun/machine_gun.gd
+++ scripts/presets/game/guns/machine_gun/machine_gun.gd
@@ -1,9 +1,19 @@
 extends Node2D
 
 onready var bullet = preload('res://presets/game/bullets/machine_bullet.tscn')
+var flipped: = false
 
 func shoot():
   var new_bullet = bullet.instance()
   new_bullet.set_position($bullet_point.get_global_position())
-  new_bullet.direction_angle = get_global_rotation()
+  var bullet_rotation: = get_global_rotation()
+  if flipped:
+    bullet_rotation += PI
+  new_bullet.direction_angle = bullet_rotation
   $bullets.add_child(new_bullet)
+
+func turn_around():
+  $skin.set_flip_v(true)
+  $skin.position.x *= -1
+  $bullet_point.position.x *= -1
+  flipped = !flipped
