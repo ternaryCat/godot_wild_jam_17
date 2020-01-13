48dcad837aaff6d99313db9a2b5170d4e07710c7
TernaryFish <sad.coder@yandex.ru>
2020-01-14 02:32:03 +0600

    add method shoot to base_gun

--- scripts/presets/game/guns/base_gun.gd
+++ scripts/presets/game/guns/base_gun.gd
@@ -4,6 +4,25 @@ class_name BaseGun
 export var is_active_enemy_trigger: = false
 export var is_active_player_trigger: = false
 var flipped: = false
+var bullet
+
+func shoot():
+  if !$delay_timer.is_stopped():
+    return
+
+  $delay_timer.start()
+  var new_bullet = bullet.instance()
+  new_bullet.set_position($bullet_point.get_global_position())
+  if is_active_enemy_trigger:
+    new_bullet.active_enemy_trigger()
+  if is_active_player_trigger:
+    new_bullet.active_player_trigger()
+
+  var bullet_rotation: = get_global_rotation()
+  if flipped:
+    bullet_rotation += PI
+  new_bullet.direction_angle = bullet_rotation
+  $bullets.add_child(new_bullet)
 
 func turn_around():
   $skin.set_flip_v(true)
--- scripts/presets/game/guns/machine_gun/machine_gun.gd
+++ scripts/presets/game/guns/machine_gun/machine_gun.gd
@@ -1,21 +1,4 @@
 extends BaseGun
 
-onready var bullet = preload('res://presets/game/bullets/machine_bullet.tscn')
-
-func shoot():
-  if !$delay_timer.is_stopped():
-    return
-
-  $delay_timer.start()
-  var new_bullet = bullet.instance()
-  new_bullet.set_position($bullet_point.get_global_position())
-  if is_active_enemy_trigger:
-    new_bullet.active_enemy_trigger()
-  if is_active_player_trigger:
-    new_bullet.active_player_trigger()
-
-  var bullet_rotation: = get_global_rotation()
-  if flipped:
-    bullet_rotation += PI
-  new_bullet.direction_angle = bullet_rotation
-  $bullets.add_child(new_bullet)
+func _ready():
+  bullet = preload('res://presets/game/bullets/machine_bullet.tscn')
